6907c8c4e08d9b0b0d4d5d1313c845f343d43620
Marco Leogrande <dark.knight.ita@gmail.com>
2024-09-10 15:35:32 -0700

    Implement zoom in/zoom out

--- neutron-heatmap/neutron-heatmap.py
+++ neutron-heatmap/neutron-heatmap.py
@@ -22,6 +22,8 @@ import gzip
 import json
 import pandas as pd
 import plotly.express as px
+import re
+from typing import List, Optional, Tuple
 
 
 def parse_json(input_file: str):
@@ -48,6 +50,22 @@ def print_cmd_help():
     )
 
 
+def parse_coordinates(cmd: str) -> Optional[List[Tuple[int, int]]]:
+    if len(cmd) <= 5 or cmd[0:5] != "zoom ":
+        return None
+    matches = re.fullmatch("(?i:zoom) ([-+0-9]*),([-+0-9]*) ([-+0-9]*),([-+0-9]*)", cmd)
+    if matches is None:
+        return None
+    try:
+        x_0 = int(matches[1])
+        z_0 = int(matches[2])
+        x_1 = int(matches[3])
+        z_1 = int(matches[4])
+    except:
+        return None
+    return [(x_0, z_0), (x_1, z_1)]
+
+
 def request_loop(all_systems):
     # Select all systems by default
     selected_systems = all_systems
@@ -67,12 +85,38 @@ def request_loop(all_systems):
         if cmd == "?" or cmd == "h" or cmd == "help":
             print_cmd_help()
         elif cmd == "display":
+            # Harcoded number of X and Y bins to avoid rendering charts too large
             px.density_heatmap(
                 selected_systems, x="coords.x", y="coords.z", nbinsx=1000, nbinsy=1000
             ).show()
+        elif cmd == "zoom":
+            # Exact match on the 'zoom' command
+            print("Zooming out")
+            selected_systems = all_systems
         elif cmd[0:4] == "zoom":
-            # norm = norm.query("`coords.x` > -1111").query("`coords.x` < 8980").query("`coords.z` > 57338").query("`coords.z` < 65269")
-            print("zoom")
+            coordinates = parse_coordinates(cmd)
+            if coordinates is None:
+                print_cmd_help()
+            else:
+                print(
+                    "Zooming to area between coordinates (%d,%d) and (%d,%d)"
+                    % (
+                        coordinates[0][0],
+                        coordinates[0][1],
+                        coordinates[1][0],
+                        coordinates[1][1],
+                    )
+                )
+                min_x = min(coordinates[0][0], coordinates[1][0])
+                max_x = max(coordinates[0][0], coordinates[1][0])
+                min_z = min(coordinates[0][1], coordinates[1][1])
+                max_z = max(coordinates[0][1], coordinates[1][1])
+                selected_systems = (
+                    all_systems.query(f"`coords.x` > {min_x}")
+                    .query(f"`coords.x` < {max_x}")
+                    .query(f"`coords.z` > {min_z}")
+                    .query(f"`coords.z` < {max_z}")
+                )
         elif cmd == "exit":
             break
         else:
