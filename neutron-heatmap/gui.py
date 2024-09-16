c4cb9558ccbb669e01fdea626f3c95e56366ac6d
Marco Leogrande <dark.knight.ita@gmail.com>
2024-09-15 22:02:06 -0700

    Refactor the GUI class to create the main window in a separate function

--- neutron-heatmap/gui.py
+++ neutron-heatmap/gui.py
@@ -36,6 +36,79 @@ class GUI(Logger):
         # Separate thread where long-operations are offloaded to
         self._longop_thread = None
 
+    def _setup_window(self):
+        # Setup root (main application window)
+        self._root = tkinter.Tk()
+        self._root.title("Neutron Star heatmap plotter")
+
+        # Setup main frame (covering the entire application window)
+        self._mainframe = ttk.Frame(self._root, padding="3 3 12 12")
+        self._mainframe.grid(
+            column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S)
+        )
+        self._mainframe.bind("<<LongOpComplete>>", self._long_op_post)
+        self._root.columnconfigure(0, weight=1)
+        self._root.rowconfigure(0, weight=1)
+
+        # Setup all actions
+        #
+        # The first column will include buttons, while the second will
+        # have labels; to preserve the layout when the window is
+        # stretched (e.g. when the status bar has a very long text),
+        # ensure that the first column never resizes, while the second
+        # one always will.
+        self._mainframe.columnconfigure(1, weight=0)
+        self._mainframe.columnconfigure(2, weight=1)
+        # 1. Display
+        ttk.Button(
+            self._mainframe,
+            text="Display",
+            command=functools.partial(self._handle_long_op, self._systems.display),
+        ).grid(column=1, row=1, sticky=tkinter.E)
+        ttk.Label(
+            self._mainframe,
+            text="open heatmap in a browser window using current zoom settings",
+        ).grid(column=2, row=1, sticky=tkinter.W)
+        # 2. Interstitial
+        ttk.Label(
+            self._mainframe,
+            text="NOTE: Zoom in/out are effective at the next 'Display'",
+        ).grid(column=1, row=2, columnspan=2, sticky=tkinter.W)
+        # 3. Zoom In
+        ttk.Button(
+            self._mainframe,
+            text="Zoom In",
+            command=functools.partial(self._handle_long_op, self._handle_zoom_in),
+        ).grid(column=1, row=3, sticky=tkinter.E)
+        zoomin_data_frame = self._build_zoomin_data_frame(self._mainframe)
+        zoomin_data_frame.grid(
+            column=2, row=3, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S)
+        )
+        # 4. Zoom Out
+        ttk.Button(
+            self._mainframe, text="Zoom Out", command=self._systems.zoom_out
+        ).grid(column=1, row=4, sticky=tkinter.E)
+        ttk.Label(self._mainframe, text="zoom out to the initial state").grid(
+            column=2, row=4, sticky=tkinter.W
+        )
+
+        # Add a progress bar for long operations; this will be created on-demand
+        self._progressbar = None
+
+        # Add a homebrew status bar at the bottom
+        self._status_text = tkinter.StringVar()
+        ttk.Label(
+            self._mainframe,
+            textvariable=self._status_text,
+            relief="sunken",
+        ).grid(column=1, row=6, columnspan=2, sticky=(tkinter.W, tkinter.E))
+
+        # Setup padding for all children of the mainframe
+        for child in self._mainframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
+        self.log("Main window ready")
+
     # Implement the Logger interface.
     def log(self, s: str):
         msg = "%s: %s" % (time.strftime("%Y-%m-%d %H:%M:%S"), s)
@@ -148,78 +221,12 @@ class GUI(Logger):
     def request_loop(self, systems: Systems):
         self._systems = systems
 
-        # Setup root (main application window)
-        root = tkinter.Tk()
-        root.title("Neutron Star heatmap plotter")
+        # Setup main window
+        self._setup_window()
 
-        # Setup main frame (covering the entire application window)
-        self._mainframe = ttk.Frame(root, padding="3 3 12 12")
-        self._mainframe.grid(
-            column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S)
-        )
-        self._mainframe.bind("<<LongOpComplete>>", self._long_op_post)
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        # Call the main event loop
+        self._root.mainloop()
 
-        # Setup all actions
-        #
-        # The first column will include buttons, while the second will
-        # have labels; to preserve the layout when the window is
-        # stretched (e.g. when the status bar has a very long text),
-        # ensure that the first column never resizes, while the second
-        # one always will.
-        self._mainframe.columnconfigure(1, weight=0)
-        self._mainframe.columnconfigure(2, weight=1)
-        # 1. Display
-        ttk.Button(
-            self._mainframe,
-            text="Display",
-            command=functools.partial(self._handle_long_op, self._systems.display),
-        ).grid(column=1, row=1, sticky=tkinter.E)
-        ttk.Label(
-            self._mainframe,
-            text="open heatmap in a browser window using current zoom settings",
-        ).grid(column=2, row=1, sticky=tkinter.W)
-        # 2. Interstitial
-        ttk.Label(
-            self._mainframe,
-            text="NOTE: Zoom in/out are effective at the next 'Display'",
-        ).grid(column=1, row=2, columnspan=2, sticky=tkinter.W)
-        # 3. Zoom In
-        ttk.Button(
-            self._mainframe,
-            text="Zoom In",
-            command=functools.partial(self._handle_long_op, self._handle_zoom_in),
-        ).grid(column=1, row=3, sticky=tkinter.E)
-        zoomin_data_frame = self._build_zoomin_data_frame(self._mainframe)
-        zoomin_data_frame.grid(
-            column=2, row=3, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S)
-        )
-        # 4. Zoom Out
-        ttk.Button(
-            self._mainframe, text="Zoom Out", command=self._systems.zoom_out
-        ).grid(column=1, row=4, sticky=tkinter.E)
-        ttk.Label(self._mainframe, text="zoom out to the initial state").grid(
-            column=2, row=4, sticky=tkinter.W
-        )
-
-        # Add a progress bar for long operations; this will be created on-demand
-        self._progressbar = None
-
-        # Add a homebrew status bar at the bottom
-        self._status_text = tkinter.StringVar()
-        ttk.Label(
-            self._mainframe,
-            textvariable=self._status_text,
-            relief="sunken",
-        ).grid(column=1, row=6, columnspan=2, sticky=(tkinter.W, tkinter.E))
-
-        # Setup padding for all children of the mainframe
-        for child in self._mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        self.log("Main window ready")
-        root.mainloop()
         self.log("Exiting...")
         # Wait for the background thread, if it is still running
         if self._longop_thread is not None:
